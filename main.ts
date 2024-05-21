c589304d1e98296c882502633d56afd6eb40661f
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-05-21 16:06:58 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,48 @@
+input.onButtonPressed(Button.A, function () {
+    if (dood == 0) {
+        voeding += 5
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (dood == 0) {
+        knuffel += 5
+    }
+})
+let dood = 0
 let knuffel = 20
 let voeding = 20
-let dood = 0
+dood = 0
+loops.everyInterval(1000, function () {
+    knuffel += -1
+    voeding += -1
+})
+basic.forever(function () {
+    if (knuffel > 10 && voeding > 10) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (knuffel < 10 && knuffel > 0 && (voeding < 10 && voeding > 0)) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+    if (knuffel < 0 && voeding < 0) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        dood = 1
+    }
+})
